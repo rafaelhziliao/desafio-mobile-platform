5fc746e540ed656c92691a91a4254abbf23a0dd7
Rafael Ziliao <rafael.ziliao@meupag.com.br>
2021-06-18 15:25:35 -0300

    Get swiftlint from pods folder

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-"${PODS_ROOT}/SwiftLint/swiftlint"
+if which "${PODS_ROOT}/SwiftLint/swiftlint" >/dev/null; then
+    ${PODS_ROOT}/SwiftLint/swiftlint
+else
+    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
