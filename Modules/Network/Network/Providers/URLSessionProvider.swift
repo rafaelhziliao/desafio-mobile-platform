677f25642a2c462946139f19d40734ee42e862ac
Rafael Ziliao <rafael.ziliao@meupag.com.br>
2021-06-20 17:30:43 -0300

    Add URLSessionProvider

---
+++ Modules/Network/Network/Providers/URLSessionProvider.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public final class URLSessionProvider: NetworkService, RequestHandleResponsable {
+    private let session: URLSession
+
+    public init() {
+        session = URLSession(configuration: URLSession.shared.configuration)
+    }
+
+    public func performRequest<T: Decodable>(endpoint: Endpoint, result: @escaping ResultHandler<T>) {
+        guard let request = URLRequest(endpoint: endpoint) else { preconditionFailure("Fail on create request") }
+
+        let task = session.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                let response = response as? HTTPURLResponse
+
+                self.handleResponse(
+                    data: data,
+                    response: response,
+                    error: error,
+                    result: result
+                )
+            }
+        }
+        task.resume()
+    }
+}
